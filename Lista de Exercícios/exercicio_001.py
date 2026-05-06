c60bb17528efc23ac2cd403b07075c976c6ab2e5
Kujira No Pen <guilherme.p.lobe@gmail.com>
2026-05-06 17:26:26 -0300

    Alguns comentarios
    Só foram adicionado alguns comentarios.

--- Lista de Exercícios/exercicio_001.py
+++ Lista de Exercícios/exercicio_001.py
@@ -1,2 +1,4 @@
 # Proposta: dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
 
+# A = b X h
+# A é a área, b é a base e h é a altura.
---
+++ Lista de Exercícios/exercicio_002.py
@@ -0,0 +1,2 @@
+# Proposta: 2. dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
+
