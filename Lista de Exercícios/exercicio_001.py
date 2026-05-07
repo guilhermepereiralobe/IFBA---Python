0e91360c59e146ba0aede29093bbec984f9256fd
Kujira No Pen <guilherme.p.lobe@gmail.com>
2026-05-06 23:30:40 -0300

    Tentativa de repetição.

--- Lista de Exercícios/exercicio_001.py
+++ Lista de Exercícios/exercicio_001.py
@@ -2,13 +2,20 @@
 
 # A = b X h
 # A é a área, b é a base e h é a altura.
-b = float(input('[cm] Valor da base: '))
-h = float(input('[cm] Valor da altura: '))
-a = b * h
 
-print(f'A área de um retângulo com a base {b}cm e altura de {h}cm é igual a {a}cm²')
+def calcular_ratangulo(bd, hd):
+    if bd >= 1 and hd >= 1:
+        a = bd * hd
+        print(f'A área de um retângulo com a base {bd}cm e altura de {hd}cm é igual a {a}cm²')
+    else:
+        print("Faça de novo.")
 
 # Repetição.
+while True:
+    b = float(input('[cm] Valor da base: '))
+    h = float(input('[cm] Valor da altura: '))
+    print(calcular_ratangulo(bd=b, hd=h))
+    
 # Função.
 # Validação do número.
 # Tratamento de erro.
--- Lista de Exercícios/exercicio_001_interface.py
+++ Lista de Exercícios/exercicio_001_interface.py
@@ -7,6 +7,5 @@ cg.title('Calculadora Geométrica')
 cg.geometry('600x400')
 cg.register(False, False)
 cg._set_appearance_mode("light")
-cg._disable_macos_dark_title_bar()
 
 cg.mainloop()
